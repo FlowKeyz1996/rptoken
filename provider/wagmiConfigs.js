cd834857ec99358903e2f161216200a1288a5f40
flowkeyz1996 <jonielsanni@gmail.com>
2025-11-12 11:00:37 +0100

    change wagmi file to sepolia from holesky

--- provider/wagmiConfigs.js
+++ provider/wagmiConfigs.js
@@ -1,12 +1,11 @@
-import {holesky, polygon} from "wagmi/chains";
-import {getDefaultConfig} from "@rainbow-me/rainbowkit";
-
+import { sepolia, polygon } from "wagmi/chains";
+import { getDefaultConfig } from "@rainbow-me/rainbowkit";
 
 const projectId = process.env.NEXT_PUBLIC_WALLET_CONNECT_PROJECT_ID;
 
 export const config = getDefaultConfig({
-    appName:"RPTOKEN",
-    projectId: projectId,
-    chains:[holesky],
-    ssr:true,
-})
+  appName: "RPTOKEN",
+  projectId: projectId,
+  chains: [sepolia], // 👈 switched from holesky to sepolia
+  ssr: true,
+});
